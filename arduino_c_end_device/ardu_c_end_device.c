0422a1c317a1b5b183d4bf7d2faeef369e766984
laschlo <laschlo@gmail.com>
2014-02-28 16:03:02 +0100

    small changes

--- arduino_c_end_device/ardu_c_end_device.c
+++ arduino_c_end_device/ardu_c_end_device.c
@@ -15,10 +15,14 @@
 #include <avr/interrupt.h>
 #include <string.h>
 #include <stdlib.h>
-#include "uart/uart.h"
-#include "nRF/radio.h"
-#include "nRF/stations.h"
+#include "uart.h"
+#include "radio.h"
+#include "stations.h"
 
+//set pwm pin - arduino pin9
+#define PWM_DDR			DDRB
+#define PWM_PORT		PORTB
+#define PWM_PIN			PB1
 
 #define	BTN_DDR			DDRC
 #define BTN_PORT		PORTC
@@ -31,6 +35,30 @@
 #define LIGHT1_OFF()	(LIGHT1_PORT |= _BV(LIGHT1_PIN))
 #define LIGHT1_ON()		(LIGHT1_PORT &= ~_BV(LIGHT1_PIN))
 
+
+
+
+typedef struct _device{
+	uint8_t light;
+	uint8_t pwm;
+	uint8_t type;
+	uint16_t temperature;
+}device_t;
+
+static uint8_t send_packet(device_t* tosend);
+static void update_coils(void);
+
+uint8_t controller_addr[5] = ADDRESS_CONTROLLER; // Receiver address
+uint8_t my_addr[5] = ADDRESS_KITCHEN;
+
+uint8_t static rx_status;
+radiopacket_t static packet;
+volatile uint8_t rxFlag = 0;
+volatile uint8_t button_pressed;
+volatile device_t my_device;
+
+//for convertion purpose
+char buffer[10];
 
 void add_char( char *s, char  znak){
 	unsigned char i;
@@ -59,27 +87,55 @@ char *num2hex(unsigned char num)
 	return sdigit;
 }
 
-typedef struct _device{
-	uint8_t light;
-	uint8_t pwm;
-	uint8_t type;
-	uint16_t temperature;
-}device_t;
+void lights_off(){
+	TCCR1A &= ~_BV(COM1A1) & ~_BV(COM1A0);
+	PWM_PORT |= _BV(PWM_PIN);
+}
 
-static uint8_t send_packet(device_t* tosend);
-static void update_coils(void);
+void lights_on(){
+	TCCR1A |= _BV(COM1A1) | _BV(COM1A0);
+	PWM_PORT &= ~_BV(PWM_PIN);
+}
 
-uint8_t controller_addr[5] = ADDRESS_CONTROLLER; // Receiver address
-uint8_t my_addr[5] = ADDRESS_KITCHEN;
+void smartaxe_init(){
+	//Set Button
+	BTN_DDR &= ~_BV(BTN_PIN);
+	BTN_PORT |= _BV(BTN_PIN);
+	
+	//Enable button interrupt. Note this should be changed once the pin number for button is changed in the project
+	PCICR |= _BV(PCIE1);
+	PCMSK1 |= _BV(PCINT8);
+	
+	//Set light1 pin
+//	LIGHT1_DDR |= _BV(LIGHT1_PIN);
+//	LIGHT1_OFF();
+	
+	
+	//PWM, Phase Correct, 8-bit, inverting mode since LED is ON while pin is LOW
+	//TCCR1A |= _BV(COM1A1) | _BV(COM1A0) |_BV(WGM10);
+	TCCR1A |= _BV(WGM10);
+	
+	//Prescaler set to fclk/64/255 (8bit mode) /2 (phase correct)
+	TCCR1B |= _BV(CS11) | _BV(CS10);
+	
+	//set PWM
+	PWM_DDR |= _BV(PWM_PIN);
+		
+	//PWM Duty Cycle
+	OCR1A = 0;
+	
+	Radio_Init();
+	
+	// configure the receive settings for radio pipe 0
+	Radio_Configure_Rx(RADIO_PIPE_0, my_addr, ENABLE);
+	
+	// configure radio transceiver settings.
+	Radio_Configure(RADIO_1MBPS, RADIO_HIGHEST_POWER);
 
-uint8_t rx_status;
-radiopacket_t packet;
-volatile uint8_t rxFlag = 0;
-volatile uint8_t button_pressed;
-volatile device_t my_device;
-
-
-char buffer[10];
+	//Set TX address to arduino connected to OpenHab
+	Radio_Set_Tx_Addr(controller_addr);
+	
+}
 
 int main(void)
 {	
@@ -87,33 +143,18 @@ int main(void)
 	uart_init((UART_BAUD_SELECT((BAUD),F_CPU)));	
 	sei();
 	
-	//Set Button
-	BTN_DDR &= ~_BV(BTN_PIN);
-	BTN_PORT |= _BV(BTN_PIN);
-	
-	//Enable button interrupt
-	PCICR |= _BV(PCIE1);
-	PCMSK1 |= _BV(PCINT8);	
+	smartaxe_init();
+		
 	button_pressed = 0;
+	lights_off();
 
 	
-	//Set light1 pin
-	LIGHT1_DDR |= _BV(LIGHT1_PIN);
-	LIGHT1_OFF();
+	
 	my_device.light=0;
-	my_device.pwm=255;
+	my_device.pwm=50;
 	my_device.temperature = 2545;
 	
-	 Radio_Init();
-
-	 // configure the receive settings for radio pipe 0
-	 Radio_Configure_Rx(RADIO_PIPE_0, my_addr, ENABLE);
-	 
-	 // configure radio transceiver settings.
-	 Radio_Configure(RADIO_1MBPS, RADIO_HIGHEST_POWER);	
-
-	//Set TX address to arduino connected to OpenHab
-	Radio_Set_Tx_Addr(controller_addr);
+	 	 
 	 
 	 while(1){		
 		 		
@@ -191,10 +232,11 @@ ISR(PCINT1_vect){
 
 static void update_coils(){
 	if(my_device.light){
-		LIGHT1_ON();
+		lights_on();
 	}else{
-		LIGHT1_OFF();
+		lights_off();
 	}
+	OCR1A = my_device.pwm;
 }
 
 static uint8_t send_packet(device_t* tosend){
